e5bd44129b5511257f84b48bfea80e4cb70ef0ac
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-13 20:57:24 +0100

    Created the two helper functions

--- 1.1-elements_of_programming/1.3.scm
+++ 1.1-elements_of_programming/1.3.scm
@@ -1,2 +1,8 @@
 ; Define a procedure that takes three numbers as arguments 
 ; and returns the sum of the squares of the two larger numbers.
+
+(define (square x)
+    (* x x))
+
+(define (sum-of-squares x y)
+    (+ (square x) (square y)))

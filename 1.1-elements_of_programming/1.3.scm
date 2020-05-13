54668d111535b952f28fa32d8b832e60397fab53
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-13 21:03:01 +0100

    Implemented the sum of square of the two largest numbers

--- 1.1-elements_of_programming/1.3.scm
+++ 1.1-elements_of_programming/1.3.scm
@@ -6,3 +6,9 @@
 
 (define (sum-of-squares x y)
     (+ (square x) (square y)))
+
+(define (sum-of-squares-two-largest x y z)
+    (cond
+        ((and (>= x y) (>= z y)) (sum-of-squares x z))
+        ((and (>= y z) (>= x z)) (sum-of-squares y x))
+        ((and (>= y x) (>= z x)) (sum-of-squares y z))))
